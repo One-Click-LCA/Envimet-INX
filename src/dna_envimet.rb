d6ec082cee2094c27750cc50ea1f3884518efca1
AntonelloDN <plugin@antonellodinunzio.online>
2025-01-18 18:20:58 +0100

    fix(header): remove comment auth

--- src/dna_envimet.rb
+++ src/dna_envimet.rb
@@ -2,8 +2,6 @@
 # ---------------------------------------------------------------------
 # Envimet INX: A plugin for Sketchup to write model for ENVI_MET.
 #
-# Copyright (C) 2022 ENVI-met GmbH <info@envi-met.com>
-#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
